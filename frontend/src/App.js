f3fd16d4607fbd1262a389435911d0dc42c0936b
gautamkumar <gkt2work@gmail.com>
2025-01-17 14:37:35 +0530

    Added App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Layout/Navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import Sidebar from './components/Layout/Sidebar/Sidebar';
+import Applications from './components/Applications/Application';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <div className="app-layout">
+      <Sidebar />
+      <div className="content-section">
+      </div>
     </div>
+  </div>
   );
 }
 
