3ec5c13bed546a2d8c6fa3b7aa7019c47b9c2b27
gautamkumar <gkt2work@gmail.com>
2025-01-17 14:37:20 +0530

    Added Index

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,9 @@
+import NotificationIcon from './images/notification.png';
+import AvatarIcon from './images/userIcon.jpg';
+import ApplicationIcon from './images/application-ico.jpg';
+import AnalyticsIcon from './images/analytics.jpg';
+import EditIcon from './images/edit-icon.png';
+import DeleteIcon from './images/delete-icon.png';
+import AcceptedIcon from './images/accepted-icon.jpg';
+
+export  {NotificationIcon, AvatarIcon, ApplicationIcon,AnalyticsIcon,EditIcon,DeleteIcon,AcceptedIcon} ;
