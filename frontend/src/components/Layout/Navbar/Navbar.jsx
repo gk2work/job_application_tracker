03b931e947a77e393a9431fc9f94276760498189
gautamkumar <gkt2work@gmail.com>
2025-01-17 14:35:36 +0530

    Added Navbar

--- frontend/src/components/Layout/Navbar/Navbar.jsx
+++ frontend/src/components/Layout/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import "./Navbar.css";
+import { AvatarIcon, NotificationIcon } from "../../../assets";
 
 const Navbar = () => {
-
-    return (
-        <>
-        </>
-    )
-
-}
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand">
+            <h4>JobTracker</h4>
+          </a>
+          <a className="nav-link" href="/application">
+            <h5>Applications</h5>
+          </a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2 input-searchbar"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+          <div className="d-flex gap-2">
+            <div>
+              <img height={25} src={NotificationIcon} alt="" />
+            </div>
+            <div className="avatar">
+              <img height={25} src={AvatarIcon} alt="" />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 export default Navbar;
